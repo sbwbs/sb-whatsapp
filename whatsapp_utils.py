cdd6d81b7376988321c294e48db65d2a1c23e46b
Won Suh <won.suh@won.suh-Mac>
2024-07-18 00:17:12 +0900

    updated

--- whatsapp_utils.py
+++ whatsapp_utils.py
@@ -35,7 +35,6 @@ def is_valid_whatsapp_message(body: Dict[str, Any]) -> bool:
         and body.get("entry")
         and body["entry"][0].get("changes")
         and body["entry"][0]["changes"][0].get("value")
-        and body["entry"][0]["changes"][0]["value"].get("messages")
     )
 
 def extract_whatsapp_message(body: Dict[str, Any]) -> Dict[str, Any]:
